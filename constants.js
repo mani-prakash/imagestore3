e53dd267c50ffc9295c03062cb057805d7d6b0cc
akhil29 <akhil.appana@gmail.com>
2016-05-24 22:55:16 +0530

    added mongo url

--- constants.js
+++ constants.js
@@ -1,2 +1,3 @@
-exports.MONGO_URI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/HelloMongoose';
+exports.MONGO_URI = ' mongodb://heroku_zl6h4v32:3a3dpj0eovrl5mq4vuet2dn9hm@ds011382.mlab.com:11382/heroku_zl6h4v32'
+//process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/HelloMongoose';
 exports.MONGO_PORT = process.env.PORT || 5000;
